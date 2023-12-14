475460d607587834e7beb93d5f39d339351e57ee
Uroma002 <146116449+uroma002@users.noreply.github.com>
2023-12-14 19:41:44 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,53 +1,14 @@
-def generate_random_data():
 
-        print(f"Random Number: {item}")
-    return data
-    data = [random.randint(1, 100) for _ in range(10)]
-if __name__ == "__main__":
-    for item in data:
-
-def main():
-import random
-
-
-    data = generate_random_data()
-    main()    for item in data:
     return data
 
-if __name__ == "__main__":
         print(f"Random Number: {item}")
-def generate_random_data():
-
-
-
-    data = [random.randint(1, 100) for _ in range(10)]
     main()
 def main():
 import random
-    data = generate_random_data()if __name__ == "__main__":
-
-
-    data = [random.randint(1, 100) for _ in range(10)]
 def generate_random_data():
-    for item in data:
-
     data = generate_random_data()
-import random
 
-    main()
-    return data
-def main():
-        print(f"Random Number: {item}")
     for item in data:
     data = [random.randint(1, 100) for _ in range(10)]
 
 if __name__ == "__main__":
-def generate_random_data():
-import random
-        print(f"Random Number: {item}")
-
-    return data
-def main():
-
-    data = generate_random_data()
-    main()
